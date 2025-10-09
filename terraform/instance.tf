34b349d3d8bc9b68b181af1ed16b6f52cb704fa4
suraj <surajbele51@gmail.com>
2025-10-09 09:42:11 +0530

    terraform 1st file

---
+++ terraform/instance.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "myec2" {
+ ami =  "ami-0bbdd8c17ed981ef9"
+ instance_type = "t2.micro"
+ vpc_security_group_ids = ["sg-0582ccf7b37e6f97e"]
+ key_name = "id_rsa"
+ tags = {
+   name = "myec2"
+   env = "dev"
+ }
+}
